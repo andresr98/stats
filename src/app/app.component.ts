e488291cf4779c8c314b3949e33b689cd4d1b968
Andres Ruiz Graciano <aandres.r98@gmail.com>
2019-01-31 17:00:25 -0500

    change Login UI, add: validation before login, and change dependencies MDBootstrap Angular 7.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Router} from '@angular/router';
 })
 export class AppComponent {
   title = 'app';
+
+  isLogin(){
+    return (localStorage.getItem('isLogin')==='1') ? true : false;
+  }
 }
