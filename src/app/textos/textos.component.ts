d8e298c0e337d7fd9064fede384085b12ed5df5c
Nilton Vélez <nilton.steveen@gmail.com>
2019-12-10 19:54:56 -0500

    .

--- src/app/textos/textos.component.ts
+++ src/app/textos/textos.component.ts
@@ -21,6 +21,7 @@ export class TextosComponent implements OnInit {
     { value: "2", label: "2" },
     { value: "3", label: "3" },
     { value: "4", label: "4" },
+
     { value: "5", label: "5" }
   ];
   public operations = [
