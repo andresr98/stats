9b48cd70aeb353126b9c733fb812036cc780aa4f
Nilton Vélez <nilton.steveen@gmail.com>
2019-12-10 19:53:34 -0500

    - Se añade libreria de mdbootstrap como un archivo y no con token, de esta manera no hay necesidad de volver a actualizar la versión de angular (si asi se desea)

--- src/app/textos/textos.component.ts
+++ src/app/textos/textos.component.ts
@@ -1,127 +1,136 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpService } from '../http.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { HttpService } from "../http.service";
 
 @Component({
-  selector: 'app-textos',
-  templateUrl: './textos.component.html',
-  styleUrls: ['./textos.component.scss']
+  selector: "app-textos",
+  templateUrl: "./textos.component.html",
+  styleUrls: ["./textos.component.scss"]
 })
 export class TextosComponent implements OnInit {
-	public arrayHistorias:any;
-  public actionText:string;
+  public arrayHistorias: any;
+  public actionText: string;
+  public info: boolean = false;
   public onEdit;
   public btnText;
-  public pets=[
-      { value: 'Perro', label: 'Perro' },
-      { value: 'Gato', label: 'Gato' },
+  public pets = [
+    { value: "Perro", label: "Perro" },
+    { value: "Gato", label: "Gato" }
   ];
-  public levels=[
-      { value: '1', label: '1' },
-      { value: '2', label: '2' },
-      { value: '3', label: '3' },
-      { value: '4', label: '4' },
-      { value: '5', label: '5' }
+  public levels = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+    { value: "5", label: "5" }
   ];
-  public operations=[
-      { value: 'Suma', label: 'Suma' },
-      { value: 'Resta', label: 'Resta' },
+  public operations = [
+    { value: "Suma", label: "Suma" },
+    { value: "Resta", label: "Resta" }
   ];
   public opSelected;
   public levelSelected;
-  public petSelected:string;
+  public petSelected: string;
   public txtDescripcion;
   public onDelete;
-  public need="";
+  public need = "";
 
-
-  @ViewChild('form') public contentModal;
-  constructor(private _service: HttpService) { }
+  @ViewChild("form") public contentModal;
+  constructor(private _service: HttpService) {}
 
   ngOnInit() {
-  this._service.getHistorias().subscribe(res => {
-  		this.arrayHistorias = res;
-  	},err=>{
-  		console.log(err);
-  	});
-  }
-
-  setDescripcion(des){
-    this.onEdit=des;
-    this.txtDescripcion=des.descripcion;
-    this.btnText='Editar';
-    this.actionText='Editar Historia';
-    this.need='No es necesario llenar todos los campos.'
-    this.petSelected="Seleccione una mascota";
-    this.opSelected=(des.tipoOperacion==0)?"Suma":"Resta";
-    this.levelSelected=des.nivel;
-    this.petSelected=des.tipoMascota;
-  }
-
-
-
-  updateText(){
-   // this.txtDescripcion=this.txtDescripcion.replace(/\n/g, "*");
-   if(this.btnText=='Agregar'){
-    let op = (this.opSelected=='Suma')?0:1;
-    let obj={
-      "tipoTexto": 1,
-      "habilitado": true,
-      "descripcion": this.txtDescripcion,
-      "tipoMascota":this.petSelected,
-      "nivel": parseInt(this.levelSelected),
-      "tipoOperacion": op
-    };
-    console.log(obj);
-      this._service.agregarHistoria(obj).subscribe(res => {
-        console.log(res);
-        this.arrayHistorias.push(res);
-        this.contentModal.hide();
-        this.txtDescripcion='';
-      }, err => {
+    this._service.getHistorias().subscribe(
+      res => {
+        this.arrayHistorias = res;
+      },
+      err => {
         console.log(err);
-      });
-   }else{
-    let op = (this.opSelected=='Suma')?0:1;
-    let obj={
-      "id": this.onEdit['id'],
-      "descripcion": this.txtDescripcion,
-      "tipoTexto": 1,
-      "tipoMascota":this.petSelected,
-      "nivel": parseInt(this.levelSelected),
-      "tipoOperacion": op
-    };
-    console.log(obj);
-    this._service.actualizarHistoria(obj).subscribe(res => {
-        console.log(res);
-        this.onEdit['descripcion']=res['descripcion'];
-        this.onEdit['tipoMascota']=res['tipoMascota'];
-        this.onEdit['tipoOperacion']=res['tipoOperacion'];
-        this.onEdit['nivel']=res['nivel'];
-        this.contentModal.hide();
-        this.txtDescripcion='';
-      }, err => {
-        console.log(err);
-      });
-   }
-
+      }
+    );
   }
 
-  borrarHistoria(item){
-    this.onDelete=item;
-    var deleted=this.onDelete;
+  setDescripcion(des) {
+    this.onEdit = des;
+    this.txtDescripcion = des.descripcion;
+    this.btnText = "Editar";
+    this.actionText = "Editar Historia";
+    this.need = "No es necesario llenar todos los campos.";
+    this.petSelected = "Seleccione una mascota";
+    this.opSelected = des.tipoOperacion == 0 ? "Suma" : "Resta";
+    this.levelSelected = des.nivel;
+    this.petSelected = des.tipoMascota;
+  }
+
+  updateText() {
+    // this.txtDescripcion=this.txtDescripcion.replace(/\n/g, "*");
+    if (this.btnText == "Agregar") {
+      let op = this.opSelected == "Suma" ? 0 : 1;
+      let obj = {
+        tipoTexto: 1,
+        habilitado: true,
+        descripcion: this.txtDescripcion,
+        tipoMascota: this.petSelected,
+        nivel: parseInt(this.levelSelected),
+        tipoOperacion: op
+      };
+      console.log(obj);
+      this._service.agregarHistoria(obj).subscribe(
+        res => {
+          console.log(res);
+          this.arrayHistorias.push(res);
+          this.contentModal.hide();
+          this.txtDescripcion = "";
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    } else {
+      let op = this.opSelected == "Suma" ? 0 : 1;
+      let obj = {
+        id: this.onEdit["id"],
+        descripcion: this.txtDescripcion,
+        tipoTexto: 1,
+        tipoMascota: this.petSelected,
+        nivel: parseInt(this.levelSelected),
+        tipoOperacion: op
+      };
+      console.log(obj);
+      this._service.actualizarHistoria(obj).subscribe(
+        res => {
+          console.log(res);
+          this.onEdit["descripcion"] = res["descripcion"];
+          this.onEdit["tipoMascota"] = res["tipoMascota"];
+          this.onEdit["tipoOperacion"] = res["tipoOperacion"];
+          this.onEdit["nivel"] = res["nivel"];
+          this.contentModal.hide();
+          this.txtDescripcion = "";
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  borrarHistoria(item) {
+    this.onDelete = item;
+    var deleted = this.onDelete;
     console.log("--------------");
     console.log(this.onDelete);
     console.log("--------------");
-    var pos= this.arrayHistorias.indexOf(this.arrayHistorias.filter(i=>{
-        return i.id==item.id;
-      })[0]);
-     this._service.borrarHistoria(deleted).subscribe(res => {
-      console.log(res);
-      this.arrayHistorias.splice(pos, 1);
-    },err=>{
-      console.log(err);
-    });
+    var pos = this.arrayHistorias.indexOf(
+      this.arrayHistorias.filter(i => {
+        return i.id == item.id;
+      })[0]
+    );
+    this._service.borrarHistoria(deleted).subscribe(
+      res => {
+        console.log(res);
+        this.arrayHistorias.splice(pos, 1);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-
-
 }
